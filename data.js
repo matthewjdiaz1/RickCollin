af6687876079199ec148e41f68f8250cd1d2c80c
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2019-11-09 18:58:50 -0800

    refactored data.js

--- data.js
+++ data.js
@@ -1,3 +1,15 @@
 const data = {
-
+  "flavors": [
+    {
+      name: `Raspberry Sorbet`,
+      info: `We blended delicious, ripe raspberries into a
+      smooth puree for this tangy yet sweet fruit
+      sorbet. It's refreshing and smooth with a
+      sweet flavor intensity.`,
+      primaryColor: "rgb(241,168,182)",
+      secondaryColor: "rgb(241,135,149)",
+    },
+  ]
 }
+
+module.exports = data;
