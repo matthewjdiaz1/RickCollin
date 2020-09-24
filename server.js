ea8deca49d9af28d96c49f50fad00ac5758cbbda
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-09-24 11:14:12 -0700

    refactored server for deployment

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const port = 3000;
+const publicPath = path.join(__dirname, '..', 'RickCollin/public');
+const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
-app.listen(port, () => console.log(`App listening on port ${port}.`));
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log('Server is up!', port);
+});
