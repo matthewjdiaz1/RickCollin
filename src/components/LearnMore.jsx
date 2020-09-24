cfc919cf5d1bc14c8628348f0e9ce08fbee01a08
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-09-24 10:53:21 -0700

    removed console.log

--- src/components/LearnMore.jsx
+++ src/components/LearnMore.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 class LearnMore extends React.Component {
   render() {
-    console.log(this.props);
     return (
       <div className="icecream-learn-more" style={{ backgroundColor: this.props.hoverColor }}>
         Learn More
